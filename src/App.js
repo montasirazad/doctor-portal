dfe40baf266fe9ac22bd43682c67b38245885340
unknown <montasirazad@gmail.com>
2022-01-07 08:36:21 +0600

    bootstrap prob solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div >
-      
+      <Routes>
+        <Route path='' element={<Home />} />
+      </Routes>
     </div>
   );
 }
